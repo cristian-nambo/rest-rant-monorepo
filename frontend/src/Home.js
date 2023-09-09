5ea4af4fd4ef082d37c2607975d1b787714c5600
cristian-nambo <cristian-nambo@hotmail.com>
2023-09-09 12:58:14 -0700

    Completed LogIn Form

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,15 +3,19 @@ function Home() {
   return (
     <main>
       <h1>HOME</h1>
-      <div >
+      <div>
+        <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center",}}>
         <img height="300" width="500" src="http://localhost:5000/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-        <div>
+        </div>
+        <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center",}}>
           Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
         </div>
       </div>
+      <div style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center",}}>
       <a href="/places">
         <button className="btn-primary">Places Page</button>
       </a>
+      </div>
     </main>
   );
 }
